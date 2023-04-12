16e4fd9cbf06df3d136eb493ec0e2a7ab3c4fa54
Igor <igor.sheplyakov@yandex.ru>
2023-04-12 12:13:17 +0300

    перенес логику mapper в классы

--- src/main/kotlin/ru/igor/stockview/dto/NewsDto.kt
+++ src/main/kotlin/ru/igor/stockview/dto/NewsDto.kt
@@ -1,9 +1,21 @@
 package ru.igor.stockview.dto
 
+import ru.igor.stockview.entity.NewsEntity
+
 data class NewsDto(
-        val id: Int? = null,
-        val title: String,
-        val description: String,
-        val newsLink: String,
-        val imageLink: String,
-)
+    val id: Int? = null,
+    val title: String,
+    val description: String,
+    val newsLink: String,
+    val imageLink: String,
+) {
+    fun toEntity(): NewsEntity {
+        return NewsEntity(
+            id = 0,
+            title = this.title,
+            description = this.description,
+            newsLink = this.newsLink,
+            imageLink = this.imageLink,
+        )
+    }
+}
--- src/main/kotlin/ru/igor/stockview/entity/NewsEntity.kt
+++ src/main/kotlin/ru/igor/stockview/entity/NewsEntity.kt
@@ -1,24 +1,35 @@
 package ru.igor.stockview.entity
 
 import jakarta.persistence.*
+import ru.igor.stockview.dto.NewsDto
 
 @Entity
 @Table(name = "news")
 class NewsEntity(
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name = "id")
-        val id: Int = 0,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    val id: Int = 0,
 
-        @Column(name = "title")
-        val title: String = "",
+    @Column(name = "title")
+    val title: String = "",
 
-        @Column(name = "description")
-        val description: String = "",
+    @Column(name = "description")
+    val description: String = "",
 
-        @Column(name = "news_link")
-        val newsLink: String = "",
+    @Column(name = "news_link")
+    val newsLink: String = "",
 
-        @Column(name = "image_link")
-        val imageLink: String = ""
-)
+    @Column(name = "image_link")
+    val imageLink: String = ""
+) {
+    fun toDto(): NewsDto {
+        return NewsDto(
+            id = this.id,
+            title = this.title,
+            description = this.description,
+            newsLink = this.newsLink,
+            imageLink = this.imageLink
+        )
+    }
+}
